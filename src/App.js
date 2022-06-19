d752b28368f9e76d6b316f47bf98a790d741771e
Njies12 <habibounjie194@gmail.com>
2022-06-19 16:42:21 -0700

    using services data in json format

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import axios from "axios";
+import axios from "axios";
 import Header from "./component/Header";
 import Services from "./component/Services";
 import { useState,useEffect } from "react";
@@ -6,32 +6,13 @@ import Button from "./component/Button";
 
 
 const App =()=> {
-const [services, setServices]=useState([
-  {
-    "id":"1",
-    "logo":"",
-    "title":"Product Design",
-    "description":"Let me help you fucus on the core of web. validate the design solution,collect your wn data, understand your users, and do not copy your competitor"
-  },
-  {
-    "id":"2",
-    "logo":"",
-    "title":"Front-end Engineering",
-    "description":"Let me help you fucus on the core of web. validate the design solution,collect your wn data, understand your users, and do not copy your competitor"
-  },
-  {
-    "id":"3",
-    "logo":"",
-    "title":"Teaching",
-    "description":"Let me help you fucus on the core of web. validate the design solution,collect your wn data, understand your users, and do not copy your competitor"
-  }
-])
+const [services, setServices]=useState([])
 // this is for the db.json part
-// useEffect(()=>{
-//   axios.get(' http://localhost:4000/services').then(response=>{
-//     setServices(response.data)
-//   })
-// },[])
+useEffect(()=>{
+  axios.get(' http://localhost:4000/services').then(response=>{
+    setServices(response.data)
+  })
+},[])
  
   return ( 
     <div className="App">
