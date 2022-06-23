493b25405c9eea6de1adbe7c9b16509c0aa65e29
Njies12 <habibounjie194@gmail.com>
2022-06-22 17:34:32 -0700

    modification of app.js and index.css

--- src/App.js
+++ src/App.js
@@ -5,24 +5,52 @@ import { useState,useEffect } from "react";
 import Button from "./component/Button";
 
 
+
 const App =()=> {
 const [services, setServices]=useState([])
 // this is for the db.json part
 useEffect(()=>{
   axios.get(' http://localhost:4000/services').then(response=>{
     setServices(response.data)
+
   })
 },[])
  
   return ( 
     <div className="App">
     
-      <Header mainLogo={'Services'} subLogo={'My core special services'}/>
-      <Services services={services}/>
+       <Header mainLogo={'Services'} subLogo={'My core special services'}/>
+       <Services services={services}/>
       
-      <Button text1={'Learn More'} text2={'Hire Me'} optionKey={'or'}/>
+       <Button text1={'Learn More'} text2={'Hire Me'} optionKey={'or'}/>
+    
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+//for the data in an arry
+
+// {
+//   id:1,
+//   logo:'',
+//   title:'Product Design',
+//   description:'Let me help you fucus on the core of web. validate the design solution,collect your wn data, understand your users, and do not copy your competitor'
+// },
+// {
+//   id:1,
+//   logo:'',
+//   title:'Front-end Engineering',
+//   description:'Let me help you fucus on the core of web. validate the design solution,collect your wn data, understand your users, and do not copy your competitor'
+// },
+// {
+//   id:3,
+//   log:'',
+//   title:'Teaching',
+//   description:'Let me help you fucus on the core of web. validate the design solution,collect your wn data, understand your users, and do not copy your competitor'
+// }
